2fdab88e3d514be86a9a4b9f32ec5aa0cb5a8340
unknown <unknown>
2025-10-09 22:45:21 +0500

    fix

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ import puppeteer from "puppeteer";
 
 import { PAGE_URL, PAGE_URL_2 } from "./constant.js";
 
+const loginFieldsKeys = {
+  "input[name='email']": "login@yopmail.com",
+  "input[name='password']": "123456789",
+};
+
+const registerFieldsKeys = {
+  "input[name='firstName']": "John",
+  "input[name='lastName']": "Doe",
+  "input[name='email']": "login@yopmail.com",
+  "input[name='contactNumber']": "+441234566123",
+  "input[name='description']": "descr",
+  "input[name='crn']": "122344",
+  "input[name='title']": "2313",
+  "input[name='companyLogo']": "/path/to/your/document.pdf",
+};
+
 const setup = async (pageLink = PAGE_URL_2, options = { headless: false }) => {
   const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
@@ -24,22 +40,18 @@ const waitFor = async (page, selector) => {
 
 const submitSimpleForm = async (
   page,
-  fields = {
-    email: { selector: "input[name='email']", value: "login@yopmail.com" },
-    password: {
-      selector: "input[name='password']",
-      value: "123456789",
-    },
-  },
+  fields = loginFieldsKeys,
   button = {
     selector: "button[type='submit']",
   },
 ) => {
   try {
-    await page.type(fields?.email?.selector, fields?.email?.value);
-    await page.type(fields?.password?.selector, fields?.password?.value);
+    for (const key in fields) {
+      await page.type(key, fields?.[key]);
+    }
 
     await page.click(button.selector);
+
     await page.waitForNavigation();
   } catch (error) {
     throw error;
