e32899ef16a41b5efc9757e8d05d477371bed6de
nouman.ishtiaq.130@gmail.com <nouman.ishtiaq.130@gmail.com>
2025-11-14 00:15:36 +0500

    fix

--- src/hooks/use-mutation-api.tsx
+++ src/hooks/use-mutation-api.tsx
@@ -1,26 +1,28 @@
 import { errorSnackbar, successSnackbar } from "@/libs/snackbar";
+import { useCallback } from "react";
 
 export const useMutationApi = (props: any) => {
   const {
     apiQuery,
-    apiPaylaod,
+    apiPayload,
     successMessage = "",
     errorMessage = undefined,
   } = props;
 
   const [apiQueryTrigger, apiQueryResult] = apiQuery();
 
-  const hasReset = typeof apiQueryResult.reset === "function";
-  const handleQuery = async () => {
+  const hasReset = typeof apiQueryResult?.reset === "function";
+
+  const handleQuery = useCallback(async () => {
     try {
-      const response = await apiQueryTrigger(apiPaylaod)?.unwrap();
+      const response = await apiQueryTrigger(apiPayload)?.unwrap();
       successSnackbar(response?.data?.message || successMessage);
     } catch (error: any) {
       if (hasReset) {
         errorSnackbar(error?.data?.message ?? (error?.message || errorMessage));
       }
     }
-  };
+  }, [apiQueryTrigger, apiPayload, successMessage, errorMessage, hasReset]);
 
   const { isLoading, isError, isSuccess, data } = apiQueryResult;
 
